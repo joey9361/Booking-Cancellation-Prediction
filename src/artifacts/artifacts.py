6d6043c218c52a5e8552299837acf7306099b672
Joey Huang <joeysugg1234@gmail.com>
2026-06-20 16:28:09 +1200

    updated prediction artifact saving and loading

--- src/artifacts/artifacts.py
+++ src/artifacts/artifacts.py
@@ -1,13 +1,13 @@
 from pathlib import Path
-from src.config.pathing import PROJECT_ROOT
+from src.config.settings import ARTIFACTS_PATH
 import joblib
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from src.exceptions import CustomException
+from src.logger import logging
 import sys
 import json
 
-ARTIFACTS_DIR = PROJECT_ROOT / "src" / "artifacts" 
 
 ROOM_CODE_LOOKUP_FILE = "room_code_lookup.joblib"
 HISTORICAL_RATES_FILE = "historical_room_rates.joblib"
@@ -16,7 +16,7 @@ MADEBY_ENCODER_FILE = "madeby_encoder.joblib"
 def save_room_code_lookup(
     lookup: pd.DataFrame
     ) -> None:
-    path = ARTIFACTS_DIR
+    path = ARTIFACTS_PATH
     path.mkdir(parents=True, exist_ok=True)
     file_path = path / ROOM_CODE_LOOKUP_FILE
     joblib.dump(lookup, file_path)
@@ -24,7 +24,7 @@ def save_room_code_lookup(
 
 def load_room_code_lookup() -> pd.DataFrame:
     try:
-        file_path = ARTIFACTS_DIR / ROOM_CODE_LOOKUP_FILE
+        file_path = ARTIFACTS_PATH / ROOM_CODE_LOOKUP_FILE
         return joblib.load(file_path)
     except Exception as e:
         raise CustomException(f"Error loading room code lookup: {e}", sys)
@@ -32,7 +32,7 @@ def load_room_code_lookup() -> pd.DataFrame:
 def save_historical_room_rates(
     rates: pd.DataFrame,
 ) -> None:
-    path = ARTIFACTS_DIR
+    path = ARTIFACTS_PATH
     path.mkdir(parents=True, exist_ok=True)
     file_path = path / HISTORICAL_RATES_FILE
     joblib.dump(rates, file_path)
@@ -40,7 +40,7 @@ def save_historical_room_rates(
 
 def load_historical_room_rates() -> pd.DataFrame:
     try:
-        file_path = ARTIFACTS_DIR / HISTORICAL_RATES_FILE
+        file_path = ARTIFACTS_PATH / HISTORICAL_RATES_FILE
         return joblib.load(file_path)
     except Exception as e:
         raise CustomException(f"Error loading historical room rates: {e}", sys)
@@ -65,7 +65,7 @@ def load_lookup_artifacts(
 def save_madeby_encoder(
     encoder: OneHotEncoder
 ) -> None:
-    path = ARTIFACTS_DIR
+    path = ARTIFACTS_PATH
     path.mkdir(parents=True, exist_ok=True)
     file_path = path / MADEBY_ENCODER_FILE
     joblib.dump(encoder, file_path)
@@ -73,35 +73,54 @@ def save_madeby_encoder(
 
 def load_madeby_encoder() -> OneHotEncoder:
     try:
-        file_path = ARTIFACTS_DIR / MADEBY_ENCODER_FILE
+        file_path = ARTIFACTS_PATH / MADEBY_ENCODER_FILE
         return joblib.load(file_path)
     except Exception as e:
         raise CustomException(f"Error loading madeby encoder: {e}", sys)
 
     
-def save_prediction_artifacts(filedir: Path, threshold: float, model_path: str) -> None:
-    filedir.mkdir(parents=True, exist_ok=True)
-    if type(float(threshold)) != float:
-        raise TypeError(f"Threshold must be a float, got {type(threshold)}")
-    threshold_artifacts = {
-        "threshold": float(threshold),
-        "model_path": str(model_path)
-    }
-    filepath = filedir / "prediction_artifacts.json"
+def save_prediction_artifacts(
+    best_threshold: float, 
+    precision_vals: list | None, 
+    recall_vals: list | None, 
+    threshold_vals: list | None
+    ) -> None:
+    prediction_artifacts = {}
+
+    if threshold_vals is not None or precision_vals is not None or recall_vals is not None:
+        logging.info(f"Threshold tuning was performed")
+        if not isinstance(precision_vals, list):
+            raise TypeError(f"Precision values must be a list, got {type(precision_vals)}")
+        if not isinstance(recall_vals, list):
+            raise TypeError(f"Recall values must be a list, got {type(recall_vals)}")
+        if not isinstance(threshold_vals, list):
+            raise TypeError(f"Threshold values must be a list, got {type(threshold_vals)}")
+        if not (len(precision_vals) == len(recall_vals) == len(threshold_vals)):
+            raise ValueError(f"""Precision, recall, and threshold values must have the same length, 
+                                got {len(precision_vals)}, {len(recall_vals)}, {len(threshold_vals)}""")
+        logging.info(f"Precision, recall, and threshold values have the same length and correct type")
+        prediction_artifacts = {
+            "precision_vals": precision_vals,
+            "recall_vals": recall_vals,
+            "threshold_vals": threshold_vals
+        }
+
+    if type(float(best_threshold)) != float:
+        raise TypeError(f"Threshold must be a float, got {type(best_threshold)}")
+    logging.info(f"Threshold is a float and correct type")
+    prediction_artifacts['best_threshold'] = float(best_threshold)
+    
+    ARTIFACTS_PATH.mkdir(parents=True, exist_ok=True)
+    filepath = ARTIFACTS_PATH / "prediction_artifacts.json"
     with open(filepath, 'w') as f:
-        json.dump(threshold_artifacts, f)
+        json.dump(prediction_artifacts, f)
+    logging.info(f"Prediction artifacts saved to {filepath}")
 
 def load_saved_prediction_artifacts(filepath: Path | None) -> dict:
     if filepath is None:
-        filepath = ARTIFACTS_DIR / "prediction_artifacts.json"
+        filepath = ARTIFACTS_PATH / "prediction_artifacts.json"
     if not filepath.exists():
         raise FileNotFoundError(f"File {filepath} not found")
     with open(filepath, 'r') as f:
         prediction_artifacts = json.load(f)
-    if not isinstance(prediction_artifacts["threshold"], float):
-        raise TypeError(f"Threshold must be a float, got {type(prediction_artifacts["threshold"])}")
-    if not isinstance(prediction_artifacts["model_path"], str):
-        raise TypeError(f"Model path must be a string, got {type(prediction_artifacts["model_path"])}")
-    if not Path(prediction_artifacts["model_path"]).exists():
-        raise FileNotFoundError(f"Model path {prediction_artifacts["model_path"]} not found")
     return prediction_artifacts
