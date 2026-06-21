1f6412337f9469ee3aaf75b68ac792c2e99c5f71
Joey Huang <joeysugg1234@gmail.com>
2026-06-21 14:27:11 +1200

    working main pipeline draft

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,15 @@ from preprocessing import run_offline_preprocessing, full_custom_transform
 from model import initialize_model, apply_best_threshold, xgb_scale_pos_weight
 from tuning import custom_hyperparameter_tuning, custom_temporal_cv, find_best_threshold
 from evaluation import evaluate_custom_predictions
-from src.config.settings import LOAD_OFFLINE_DATA_QUERY,MODEL_PARAMS, PARAM_TUNING_GRID, GRID_SEARCH_PARAMS, MODEL_PATH, ARTIFACTS_PATH
+from src.config.settings import LOAD_OFFLINE_DATA_QUERY,MODEL_PARAMS, PARAM_TUNING_GRID, GRID_SEARCH_PARAMS, MODEL_PATH, MIN_ACCEPTED_PRECISION, ARTIFACTS_PATH
 from joblib import dump
-from src.artifacts.artifacts import load_lookup_artifacts, load_madeby_encoder, save_prediction_artifacts   
+from src.artifacts.artifacts import save_prediction_artifacts, save_threshold_artifacts, load_preprocessing_artifacts   
 from src.logger import logging
 from src.exceptions import CustomException
 import sys
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def main(
     model_type: str = 'random_forest', 
@@ -19,8 +22,7 @@ def main(
     df = datamanager.load_query(LOAD_OFFLINE_DATA_QUERY)
 
     X_train, Y_train, full_val, full_test = run_offline_preprocessing(df)
-    room_code_lookup, room_rate_lookup = load_lookup_artifacts()
-    OH_encoder = load_madeby_encoder()
+    room_code_lookup, room_rate_lookup, OH_encoder = load_preprocessing_artifacts()
     X_val, Y_val = full_custom_transform(full_val, room_code_lookup, room_rate_lookup, OH_encoder, is_offline=True)
     X_test, Y_test = full_custom_transform(full_test, room_code_lookup, room_rate_lookup, OH_encoder, is_offline=True)
 
@@ -47,10 +49,14 @@ def main(
         # threshold tuning
         probabilities = tuned_model.predict_proba(X_val)
         best_threshold, precision_vals, recall_vals, threshold_vals = find_best_threshold(probabilities, Y_val)
+        min_accepted_precision = MIN_ACCEPTED_PRECISION
+        save_threshold_artifacts(precision_vals, recall_vals, threshold_vals)
         # apply best threshold to get custom predictions
         custom_predictions, _ = apply_best_threshold(tuned_model, X_test, best_threshold)
     else:
-        best_threshold, precision_vals, recall_vals, threshold_vals = 0.5, None, None, None # default threshold
+        # delete existing threshold artifacts if threshold tuning was not performed
+        (ARTIFACTS_PATH / "threshold_artifacts.json").unlink(missing_ok=True)
+        best_threshold, min_accepted_precision = 0.5, 0.0 # default threshold and precision
         custom_predictions = tuned_model.predict(X_test)
         logging.info(f"Threshold tuning was not performed, using default threshold")
     # evaluate the model
@@ -59,7 +65,9 @@ def main(
     # save the model and its path
     MODEL_PATH.parent.mkdir(parents=True, exist_ok=True)
     dump(tuned_model, MODEL_PATH)
-    save_prediction_artifacts(best_threshold, precision_vals, recall_vals, threshold_vals)
+    save_prediction_artifacts(best_threshold, min_accepted_precision)
+
+
 if __name__ == "__main__":
     from src.testing_database import create_datamanager
     datamanager = create_datamanager()
